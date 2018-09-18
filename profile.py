06a867af5f8da8c7a1a2017e869784cc13bdd98a
bchangRep <34947323+bchangRep@users.noreply.github.com>
2018-09-17 21:02:42 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,11 +1,3 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
-Instructions:
-Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
-"""
-
 # Import the Portal object.
 import geni.portal as portal
 # Import the ProtoGENI library.
@@ -16,16 +8,36 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add XenVM to request
-node = request.XenVM("node")
+
+x = 1
+num_of_nodes = 4
+
+# Create a list of nodes, will add all existing nodes
+nodeList = list()
+
+# Append all existing nodes into nodeList
+for n in range(num_of_nodes):
+     nodeList.append(request.XenVM("node-" + str(i)))
+
 # Specify tthe disk image
-node.disk_image = "urn:publicid:IDN+emulad.net+image+emulab-ops:CENTOS7-64-STD"
+for node in nodeList:
+     node.disk_image = "urn:publicid:IDN+emulad.net+image+emulab-ops:CENTOS7-64-STD"
+     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/silly.sh"))
+     if x:
+          node.routable_control_ip = "true"
+     
+lan = request.LAN("lan)
 
-node.routable_control_ip = "true"
+# Create another list for custom IPAddresses
+address = list()
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="sudo /local/repository/silly.sh"))
+link = request.LAN("lan")
+address = list()
+for m in range(len(nodeList)):
+	    address.append (nodeList[m].addInterface("if1"))
+    	address[m].component_id = "eth1"
+	    address[m].addAddress(pg.IPv4Address("192.168.1.{}".format(str(m)),"255.255.255.0"))
+     link.addInterface(address[m])
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
